0bd95fd6efc53ae1ff54f6d9bd2e6c33cd0b8233
branch10480 <toshiharu.imaeda@gmail.com>
2021-12-27 08:45:26 +0900

    Initial Commit

---
+++ IconBeta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IconBeta
+//
+//  Created by branch10480 on 2021/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
